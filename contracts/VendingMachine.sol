3c6c532da87e8607a7dc0f5e9ffc259d989f5499
tveryakoff <tveryakov@icloud.com>
2024-02-07 22:18:04 +0000

    moved to truffle, tests added

---
+++ contracts/VendingMachine.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract VendingMachine {
+    address public owner;
+    mapping (address => uint) public  dounatBalances;
+
+    constructor() {
+        owner = msg.sender;
+        // adress(this) - contract address
+        dounatBalances[address(this)] = 100;
+    }
+
+    /// Only the owner can restock dounats!
+    error OnlyOwner();
+
+    /// You don't have enough ether! 
+    error NotEnoughEther();
+
+    /// The machine doesn't have enough dounats in stock, Sorry!
+    error NotEnoughDounats();
+
+    modifier onlyContractOwner() {
+        if (msg.sender != owner)
+            revert OnlyOwner();
+        _;
+    }
+
+    modifier hasEnoughEther(uint amount) {
+        if (msg.value <= amount * 1 ether) 
+            revert NotEnoughEther();
+            _;
+        
+    }
+
+    modifier hasEnoughDounats(uint amount) {
+        if (amount > dounatBalances[address(this)])
+            revert NotEnoughDounats(); 
+            _;
+    }
+
+
+
+    function getVendingMachineBalance() public view returns (uint) {
+        return dounatBalances[address(this)];
+    }
+
+    function restock(uint amount) public onlyContractOwner {
+        dounatBalances[address(this)] += amount;
+    }
+
+    // Can receive ether and change the state
+    function purchase(uint amount) public hasEnoughEther(amount) hasEnoughDounats(amount) payable  {
+        dounatBalances[address(this)] -= amount;
+        dounatBalances[msg.sender] += amount;
+    }
+
+}
---
+++ test/VendingMachine.js
@@ -0,0 +1,25 @@
+const VendingMachine = artifacts.require('./VendingMachine.sol')
+
+contract('VendingMachine', (accounts) => {
+    before(async() => {
+        instance = await VendingMachine.deployed()
+    })
+
+    it('ensures that the starting balance of the vending machine is 100', async () => {
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 100, 'The initial balance should be 100 donuts')
+    })
+
+    it('ensures the balance of the vending machin can be updated', async() => {
+        await instance.restock(100)
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 200, 'The balance should be 200 donuts after restocking')
+
+    })
+
+    it('allows dounats to be purchased', async () => {
+        await instance.purchase(1, {from: accounts[0], value: web3.utils.toWei('3', 'ether')})
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 199, 'The balance should be 199 donuts after sale')
+    })
+})
